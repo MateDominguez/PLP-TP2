6c033072249a89026415f8ff3893f53e9608b16b
MateDominguez <matedominguez2@gmail.com>
2025-06-14 21:03:39 -0300

    Agrega solucion ejercicios 5,7,8,9 y solucion parcial 10

--- katamino.pl
+++ katamino.pl
@@ -32,16 +32,25 @@ coordenadas(T, (I, J)) :-
     between(1, C, I),
     between(1, F, J).
     
-    
+
 % Ejercicio 5: K-Piezas
 % kPiezas(+K, -PS)
+kPiezas(K, XS) :-
+    nombrePiezas(P), 
+    kPiezasAux(K, XS, P).
 
-kPiezas(0,[]).
-kPiezas(K, [H|T]) :-
+% kPiezasAux(+K, -PS, +PRestantes)
+kPiezasAux(0,[], _). % Caso base K == 0
+kPiezasAux(K, [P|XS], [P|PS]) :- % Caso recursivo, elijo la pieza
     Km1 is K - 1,
-    nombrePiezas(Piezas),
-    member(H,Piezas),
-    kPiezas(Km1, T).
+    length(XS, Km1),
+    length(PS, LPS), % Poda si no quedan sifientes piezas
+    LPS >= Km1,
+    kPiezasAux(Km1, XS, PS).
+kPiezasAux(K, [X|XS], [_|PS]) :- % Caso recursivo, NO elijo la pieza
+    length(PS, LPS), % Poda si no quedan sifientes piezas
+    LPS >= K,
+    kPiezasAux(K, [X|XS], PS).
 
 
 % Ejercicio 6: SeccionTablero
@@ -51,11 +60,63 @@ seccionTablero(T, ALTO, ANCHO, (I,J), ST) :-
     Im1 is I - 1,
     Jm1 is J - 1,                    % Obtenemos filas y columnas
     F >= ALTO + Im1,                 % Tiene que haber sufiecientes filas
-    C >= ANCHO + Jm1,                      
-    sublista(Im1, ALTO, T, X),          % 
-    recortar_filas(Jm1, ANCHO, X, ST). 
+    C >= ANCHO + Jm1,                % Tiene que haber sufiecientes columnas
+    sublista(Im1, ALTO, T, X),       %  Me quedo con las ALTO filas a partir de i
+    recortarFilas(Jm1, ANCHO, X, ST). % Me quedo con las ANCHO columnas a partir de j
 
-recortar_filas(_, _, [], []).
-recortar_filas(J, ANCHO, [H|T], [H1|T1]) :-
+recortarFilas(_, _, [], []).
+recortarFilas(J, ANCHO, [H|T], [H1|T1]) :-
     sublista(J, ANCHO, H, H1),
-    recortar_filas(J, ANCHO, T, T1).
+    recortarFilas(J, ANCHO, T, T1).
+
+
+% Ejercicio 7: Ubicar pieza
+% ubicarPieza(+Tablero, +Identificador)
+ubicarPieza(Tablero, ID) :-
+    pieza(ID, Pieza),
+    tamano(Pieza, FilasP, ColP),
+    tamano(Tablero, FilasT, ColT),
+    coordenadas(Tablero, (I, J)),
+    I + FilasP - 1=< FilasT,
+    J + ColP - 1 =< ColT,
+    seccionTablero(Tablero, FilasP, ColP, (I,J), Seccion),
+    puedeColocar(Pieza, Seccion, ID).
+
+puedeColocar([], [], _).
+puedeColocar([P|PS], [S|SS], ID):-
+    coincideFila(P, S, ID),
+    puedeColocar(PS, SS, ID).
+
+coincideFila([], [], _).
+coincideFila([P|PS], [S|SS], ID):-
+    nonvar(P),
+    var(S),
+    S = ID,
+    coincideFila(PS, SS, ID).
+coincideFila([P|PS], [_|SS], ID):-
+    var(P),
+    coincideFila(PS, SS, ID).
+
+% Ejercicio 8: Ubicar piezas
+% ubicarPiezas(+Tablero, +Poda, +Identificadores)
+ubicarPiezas(_, _, []).
+ubicarPiezas(Tablero, Poda, [ID|IDs]):-
+    ubicarPieza(Tablero, ID),
+    poda(Poda, Tablero),
+    ubicarPiezas(Tablero, Poda, IDs).
+
+% poda(+Poda, +Tablero)
+poda(sinPoda, _).
+
+% Ejericio 9: Llenar Tablero
+% llenarTablero(+Poda, +Columnas, -Tablero)
+llenarTablero(Poda, Columnas, Tablero):-
+    tablero(Columnas, Tablero),
+    kPiezas(Columnas, Piezas),
+    ubicarPiezas(Tablero, Poda, Piezas).
+
+% Ejercicio 10: Medicion
+% cantSoluciones(+Poda, +Columnas, -N)
+cantSoluciones(Poda, Columnas, N) :-
+    findall(T, llenarTablero(Poda, Columnas, T), TS),
+    length(TS, N).
